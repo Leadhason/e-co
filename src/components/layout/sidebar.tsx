b56577f2d0a2e2d4029b7bd8050d50d126d83d4d
Leaderson <leadhason111@gmail.com>
2026-05-16 22:00:01 +0000

    update sidebar icons

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -8,7 +8,7 @@ import {
   IconUsers, 
   IconSettings,
   IconReceipt,
-  IconChart,
+  IconChartBar,
   IconChevronDown,
   IconLayoutSidebar
 } from "@tabler/icons-react";
@@ -28,7 +28,7 @@ const navGroups = [
       { name: "Categories", href: "/categories", icon: IconBox },
       { name: "Orders", href: "/orders", icon: IconReceipt },
       { name: "Customers", href: "/customers", icon: IconUsers },
-      { name: "Analytics", href: "/analytics", icon: IconChart },
+      { name: "Analytics", href: "/analytics", icon: IconChartBar },
     ]
   }
 ];
